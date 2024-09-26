e63042619a2421853f929112a70a9979d1e23f26
Widido1 <sdg-1995@protonmail.com>
2024-09-26 11:59:19 -0300

    prueba de git

--- src/app/components/banner.jsx
+++ src/app/components/banner.jsx
@@ -7,7 +7,7 @@ export default function Banner(){
         <div className="grid grid-flow-row place-self-center text-center justify-center items-center w-full theme3 px-2 font-bold italic text-3xl
             sm:grid-cols-2 sm:place-self-stretch lg:text-4xl">
             <div className="sm:w-1/3">
-                <h1><Link href="/">CozyBoys</Link></h1>
+                <h1><Link href="/">TheCozyBoys</Link></h1>
             </div>
             <div className="grid grid-flow-col gap-4 auto-cols-3 place-self-center justify-self-end justify-end text-right theme3 h-auto
             text-sm sm:text-base sm:w-2/3 md:text-lg lg:text-xl">
